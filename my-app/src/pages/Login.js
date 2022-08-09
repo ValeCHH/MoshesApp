550c1eb2e5b1aaf93895b56389367b73f72f2c64
ValeChH <93483575+ValeCHH@users.noreply.github.com>
2022-08-09 23:04:38 +0300

    Phone picker

--- my-app/src/pages/Login.js
+++ my-app/src/pages/Login.js
@@ -7,16 +7,11 @@ import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import MuiPhoneNumber from "material-ui-phone-number";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Copyright(props) {
 
-  function handleOnChangeNumber(value) {
-    this.setState({
-       phone: value
-    });
- }
+
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
@@ -55,24 +50,9 @@ export default function SignIn() {
             Sign in
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <MuiPhoneNumber
-                    name="phone"
-                    label="Phone Number"
-                    data-cy="user-phone"
-                    defaultCountry={"us"}
-                    onChange={handleOnChangeNumber}
-                  />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-         
+          <TextField  inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} />
+
+                 
             <Button
               type="submit"
               fullWidth
